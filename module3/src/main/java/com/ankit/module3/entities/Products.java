8a78574f228522f80ce52a80024b749859bb1a66
ankitydv12 <ankitydv9863@gmail.com>
2026-02-07 19:03:28 +0530

    3.1-->Spring JPA,Hibernate, Working,Constrain,Index

---
+++ module3/src/main/java/com/ankit/module3/entities/Products.java
@@ -0,0 +1,52 @@
+package com.ankit.module3.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Component
+@Entity
+@Table(
+        name = "product_table",
+        //adding constrain
+        uniqueConstraints = {
+                @UniqueConstraint(name = "sku_unique",columnNames = {"sku"}),
+                @UniqueConstraint(name = "title_price_unique",columnNames = {"name","price"})
+        },
+        //adding index
+        indexes = {
+                @Index(name = "skuIndex",columnList = "sku")
+        }
+)
+public class Products {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false , length = 20)
+    private String sku;
+
+    @Column(name = "name")
+    private String title;
+
+    private BigDecimal price;
+
+    private Integer quantity;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
