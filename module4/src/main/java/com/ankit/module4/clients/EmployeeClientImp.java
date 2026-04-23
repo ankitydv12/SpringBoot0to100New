53d60acd3ea21415b864993ce673999a4bfacb3b
ankitydv12 <ankitydv9863@gmail.com>
2026-04-23 10:28:28 +0530

    4.5 Logging

--- module4/src/main/java/com/ankit/module4/clients/EmployeeClientImp.java
+++ module4/src/main/java/com/ankit/module4/clients/EmployeeClientImp.java
@@ -4,6 +4,8 @@ import com.ankit.module4.Exception.ResourceNotFoundException;
 import com.ankit.module4.advice.ApiResponse;
 import com.ankit.module4.dto.EmployeeDTO;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
@@ -16,34 +18,49 @@ import java.util.List;
 public class EmployeeClientImp implements EmployeeClient{
 
     private final RestClient restClient;
+    private final EmployeeDTO employeeDTO;
+    Logger log = LoggerFactory.getLogger(EmployeeClientImp.class);
 
     @Override
     public List<EmployeeDTO> getAllEmployees() {
+        log.trace("Trying to Retrieve all employees Method in getAllEmployees");
         try {
+            log.info("Attempting to call the restclient method");
             ApiResponse<List<EmployeeDTO>> employeeDTOSList =  restClient.get()
                     .uri("employees")
                     .retrieve()
+                    .onStatus(HttpStatusCode::is4xxClientError,(req,res)->{
+                        log.error(new String(res.getBody().readAllBytes()));
+                        throw new RuntimeException("could not get the employee");
+                    })
                     .body(new ParameterizedTypeReference<>() {
                     });
+            log.debug("Successfully retrived the all employees ");
+            log.trace("List of employees from restclient: {}",employeeDTOSList.getData());
             return employeeDTOSList.getData();
         } catch (Exception e) {
+            log.error("Exception Occure in getAllEmployee",e);
             throw new RuntimeException(e);
         }
     }
 
     @Override
     public EmployeeDTO addEmployee(EmployeeDTO empRequest) {
+        log.trace("Enter in the addEmployee Method with request: {}",empRequest);
         try {
+            log.info("Going to Add an employeee");
             ApiResponse<EmployeeDTO> employees = restClient.post()
                     .uri("employees")
                     .body(empRequest)
                     .retrieve()
                     .onStatus(HttpStatusCode::is4xxClientError,(req,res)->{
-                        System.out.println(new String(res.getBody().readAllBytes()));
+                        log.error(new String(res.getBody().readAllBytes()));
                         throw new ResourceNotFoundException("could not create the employee");
                     })
                     .body(new ParameterizedTypeReference<>() {
                     });
+            log.debug("Adding of new emp is done");
+            log.trace("Retrieved employees list in getEmployees: {}",employees.getData());
             return employees.getData();
         } catch (Exception e) {
             throw new RuntimeException(e);
--- module4/src/test/java/com/ankit/module4/Module4ApplicationTests.java
+++ module4/src/test/java/com/ankit/module4/Module4ApplicationTests.java
@@ -1,13 +1,19 @@
 package com.ankit.module4;
 
+import com.ankit.module4.clients.EmployeeClientImp;
+import com.ankit.module4.config.RestClientConfig;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Module4ApplicationTests {
 
+	@Autowired
+	private EmployeeClientImp employeeClientImp;
 	@Test
-	void contextLoads() {
+	void getEmp() {
+		employeeClientImp.getAllEmployees();
 	}
 
 }
