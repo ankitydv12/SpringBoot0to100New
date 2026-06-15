788618f4408613794d338ace78335b6cf51ac79d
Ankit Yadav <ankitydv9863@gmail.com>
2026-06-15 19:06:48 +0530

    Customized SecurityFilterChain (WebSecurityConfig)              ,Default Behaviour , Adding Authentication ,              Global AuthenticationManager configured with UserDetailsService

--- module5SpringSecurity/src/main/java/com/ankit/module5springsecurity/services/Impl/UserServiceImpl.java
+++ module5SpringSecurity/src/main/java/com/ankit/module5springsecurity/services/Impl/UserServiceImpl.java
@@ -1,20 +1,26 @@
 package com.ankit.module5springsecurity.services.Impl;
 
 import com.ankit.module5springsecurity.entities.User;
+import com.ankit.module5springsecurity.exception.ResourceNotFoundException;
 import com.ankit.module5springsecurity.repository.UserRepository;
 import com.ankit.module5springsecurity.services.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 @Service
 @RequiredArgsConstructor
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserDetailsService {
 
     private final UserRepository userRepository;
 
+
     @Override
-    public Optional<User> getByEmail(String email) {
-        return userRepository.findByEmail(email);
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(()-> new ResourceNotFoundException("User not found"));
     }
 }
