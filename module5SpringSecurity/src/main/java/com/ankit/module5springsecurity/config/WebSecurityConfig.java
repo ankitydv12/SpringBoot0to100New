c3c6a93a96841ab76cb6dc97a36265df68c116ea
Ankit Yadav <ankitydv9863@gmail.com>
2026-06-18 19:26:02 +0530

    Commit 5.4.2 Role based Http Access

--- module5SpringSecurity/src/main/java/com/ankit/module5springsecurity/config/WebSecurityConfig.java
+++ module5SpringSecurity/src/main/java/com/ankit/module5springsecurity/config/WebSecurityConfig.java
@@ -5,6 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,8 +21,42 @@ public class WebSecurityConfig {
     {
         httpSecurity
                 .authorizeHttpRequests(auth ->
-                        auth.anyRequest().authenticated())
+                        auth
+                                .requestMatchers("/posts").permitAll() // request posts will be bypass from the authentication
+                                .requestMatchers("/post/**").hasAnyRole("ADMIN","MANAGER")
+                                .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults());
         return  httpSecurity.build();
+    }
+
+    //creating inmemory user
+    @Bean
+    public UserDetailsService inmemmoryUserDetail(){
+        UserDetails normaluser = User
+                .withUsername("abc")
+                .password(passwordEncoder().encode("abc"))
+                .roles("NORMAL_USER")
+                .build();
+
+        UserDetails admin = User
+                .withUsername("ankit")
+                .password(passwordEncoder().encode("ankit"))
+                .roles("ADMIN")
+                .build();
+
+        UserDetails manager = User
+                .withUsername("manager")
+                .password(passwordEncoder().encode("manager"))
+                .roles("MANAGER")
+                .build();
+
+        return new InMemoryUserDetailsManager(normaluser,admin,manager);
+    }
+
+    //password encoder without it spring through error
+    @Bean
+    PasswordEncoder passwordEncoder()
+    {
+        return new BCryptPasswordEncoder();
     }
 }
--- module5SpringSecurity/src/main/java/com/ankit/module5springsecurity/services/Impl/UserServiceImpl.java
+++ module5SpringSecurity/src/main/java/com/ankit/module5springsecurity/services/Impl/UserServiceImpl.java
@@ -11,7 +11,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
-@Service
+//@Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserDetailsService {
 
