4141e79f4c1e0497fb0c07cf70faea7aa9b44228
Ankit Yadav <153325875+ankitydv12@users.noreply.github.com>
2026-07-27 07:06:40 +0000

    Setting mysql in docker and postman in github vm

--- module5SpringSecurity/src/test/java/com/ankit/module5springsecurity/Module5SpringSecurityApplicationTests.java
+++ module5SpringSecurity/src/test/java/com/ankit/module5springsecurity/Module5SpringSecurityApplicationTests.java
@@ -10,16 +10,7 @@ import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 @SpringBootTest
 class Module5SpringSecurityApplicationTests {
 
-	@Autowired
-	private JwtService jwtService;
-	@Test
-	void contextLoads() {
-		User user = new User(1L,"ankit@gmail.com","123");
-		String token = jwtService.generateJwtToken(user);
-		System.out.println(token);
-
-		Long id = jwtService.getIdFromJwtToken(token);
-		System.out.printf("id: %d",id);
+	
 	}
 
-}
+
